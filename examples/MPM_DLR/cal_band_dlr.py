a6ec89f3cadf5400c8d5948b2f0eebf411feb205
leizhang <lzphy@umich.edu>
2024-10-25 11:21:07 -0400

    add MPM_DLR examples for Si bands

---
+++ examples/MPM_DLR/cal_band_dlr.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+import numpy as np
+import h5py
+import argparse
+from mpi4py import MPI
+import time
+import sys
+# path to the source directory
+sys.path.append("../../src/")
+from mini_pole_dlr import *
+
+# Initialize the MPI communicator
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+size = comm.Get_size()
+
+#------input parameters-------
+parser = argparse.ArgumentParser(description="Parameters for calculating the band structure of silicon")
+parser.add_argument("--obs"     , type=str  , default="S")
+parser.add_argument("--n0"      , type=int  , default=None)
+parser.add_argument("--err"     , type=float, default=1.e-10)
+parser.add_argument('--symmetry', type=lambda x: (str(x).lower() == 'true'), default=None)
+parser.add_argument("--np_max"  , type=int  , default=50)
+args = parser.parse_args()
+#-----------------------------
+obs = args.obs; assert obs in ["Gii", "G", "S"]
+n0  = args.n0 if args.n0 is not None else 3 if obs == "S" else 4
+err = args.err
+symmetry = args.symmetry if args.symmetry is not None else False if obs == "S" else True
+np_max = args.np_max
+
+if rank == 0:
+    t_start = time.time()
+    if obs == "S":
+        print("Observable: matrix-valued self-energy", flush=True)
+    elif obs == "G":
+        print("Observable: matrix-valued Green's function", flush=True)
+    else:
+        print("Observable: scalar-valued Green's function", flush=True)
+    print("n0 =", n0, flush=True)
+    print("error tolerance =", err, flush=True)
+    print("up-down symmetry is", symmetry, flush=True)
+
+# Read input file
+data = h5py.File("Si_dlr.h5", "r")
+if obs == "S":
+    Al_dlr = data["S/Al"][:][:, 0, :, :, :]
+    xl_dlr = data["S/xl"][:]
+else:
+    Al_dlr = data["G/Al"][:][:, 0, :, :, :]
+    xl_dlr = data["G/xl"][:]
+    if obs == "Gii":
+        Al_dlr = np.einsum("ijkk->ijk", Al_dlr)
+beta = data["beta"][()]
+data.close()
+
+# System sizes
+nk  = Al_dlr.shape[1]
+nao = Al_dlr.shape[2]
+
+# Total number of iterations
+n_iter = nk if obs == "S" or obs == "G" else nk * nao
+
+# Calculate the start and end indices for each rank
+iter_list = np.arange(rank, n_iter, size)
+if obs == "S" or obs == "G":
+    Al_i = np.zeros((np_max, nao, nao, iter_list.size), dtype=np.complex_)
+    xl_i = np.zeros((np_max, iter_list.size), dtype=np.complex_)
+    np_i = np.zeros((iter_list.size,), dtype=np.int_)
+else:
+    Al_i = np.zeros((np_max, iter_list.size), dtype=np.complex_)
+    xl_i = np.zeros((np_max, iter_list.size), dtype=np.complex_)
+    np_i = np.zeros((iter_list.size,), dtype=np.int_)
+
+# Parallelize the for loop
+for i in range(iter_list.size):
+    if obs == "S" or obs == "G":
+        k = iter_list[i]
+        p = MiniPoleDLR(Al_dlr[:, k, :, :], xl_dlr, beta=beta, n0=n0, err=err, symmetry=symmetry)
+        Al_i[:p.pole_location.size, :, :, i] = p.pole_weight
+        xl_i[:p.pole_location.size, i] = p.pole_location
+        np_i[i] = p.pole_location.size
+    else:
+        k, n = iter_list[i] // nao, iter_list[i] % nao
+        p = MiniPoleDLR(Al_dlr[:, k, n], xl_dlr, beta=beta, n0=n0, err=err, symmetry=symmetry)
+        Al_i[:p.pole_location.size, i] = p.pole_weight
+        xl_i[:p.pole_location.size, i] = p.pole_location
+        np_i[i] = p.pole_location.size
+
+# Gather data
+iter_tot = comm.gather(iter_list, root=0)
+Al = comm.gather(Al_i, root=0)
+xl = comm.gather(xl_i, root=0)
+np_tot = comm.gather(np_i, root=0)
+
+if rank == 0:
+    # Order and reshape data
+    iter_tot = np.concatenate(iter_tot, axis=0)
+    idx = np.argsort(iter_tot)
+    if obs == "S" or obs == "G":
+        np_tot = np.concatenate(np_tot, axis=0)[idx]
+        np_real_max = np.max(np_tot)
+        Al = np.concatenate(Al, axis=-1)[:, :, :, idx].reshape(np_max, nao, nao, nk)[:np_real_max, :, :, :]
+        xl = np.concatenate(xl, axis=-1)[:, idx].reshape(np_max, nk)[:np_real_max, :]
+    else:
+        np_tot = np.concatenate(np_tot, axis=0)[idx].reshape(nk, nao)
+        np_real_max = np.max(np_tot)
+        Al = np.concatenate(Al, axis=-1)[:, idx].reshape(np_max, nk, nao)[:np_real_max, :, :]
+        xl = np.concatenate(xl, axis=-1)[:, idx].reshape(np_max, nk, nao)[:np_real_max, :, :]
+    
+    # Write output file
+    data = h5py.File("pole_" + obs + ".h5", "w")
+    data["Al"] = Al
+    data["xl"] = xl
+    data["np"] = np_tot
+    data.close()
+    
+    t_end = time.time()
+    print("Finished in", t_end - t_start, "s")
---
+++ examples/MPM_DLR/plt_band_dlr.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+import argparse
+
+# Function tools
+def cal_G(z, Al, xl):
+    G_z = 0.0
+    for i in range(xl.size):
+        G_z += Al[i] / (z - xl[i])
+    return G_z
+
+def cal_G_vector(z, Al, xl):
+    G_z = 0.0
+    for i in range(xl.size):
+        G_z += Al[[i]] / (z.reshape(-1, 1) - xl[i])
+    return G_z
+
+#------input parameters-------
+parser = argparse.ArgumentParser(description="Parameters for plotting the band structure of silicon")
+parser.add_argument("--obs"     , type=str  , default="S")
+parser.add_argument("--w_min"   , type=float, default=-12)
+parser.add_argument("--w_max"   , type=float, default=12)
+parser.add_argument("--n_w"     , type=int  , default=200)
+parser.add_argument("--eta"     , type=float, default=0.005)
+args = parser.parse_args()
+#-----------------------------
+obs = args.obs; assert obs in ["Gii", "G", "S"]
+w_min = args.w_min
+w_max = args.w_max
+n_w = args.n_w
+eta = args.eta; assert eta >= 0.0
+
+# Obtain the grid for the real frequency
+HartreeToEv = 27.2114
+y = np.linspace(w_min, w_max, n_w)
+freqs = y / HartreeToEv
+
+# Read necessary data
+data = h5py.File("Si_dlr.h5", "r")
+Fk = data["Fk"][:][0]
+mu = data["mu"][()]
+path = data["hs_path/path"][:]
+sp_points = data["hs_path/sp_points"][:]
+sp_labels = data["hs_path/sp_labels"][:]
+sp_labels = [x.decode('utf-8') for x in sp_labels]
+data.close()
+
+if obs == "Gii":
+    # Read complex pole information
+    data = h5py.File("pole_Gii.h5", "r")
+    Al_Gii = data["Al"][:]
+    xl_Gii = data["xl"][:]
+    np_Gii = data["np"][:]
+    data.close()
+    # Obtain band structure
+    nk  = Al_Gii.shape[1]
+    nao = Al_Gii.shape[2]
+    DOS_G_dlr_diag = np.zeros((freqs.size, nk, nao), dtype=np.float_)
+    for k in range(nk):
+        for n in range(nao):
+            DOS_G_dlr_diag[:, k, n] = -1.0 / np.pi * cal_G(freqs + 1j * eta, Al_Gii[:np_Gii[k, n], k, n], xl_Gii[:np_Gii[k, n], k, n]).imag
+    band = DOS_G_dlr_diag.sum(axis=2)
+elif obs == "G":
+    # Read complex pole information
+    data = h5py.File("pole_G.h5", "r")
+    Al_G = data["Al"][:]; Al_G = np.einsum("ijjk->ijk", Al_G)
+    xl_G = data["xl"][:]
+    np_G = data["np"][:]
+    data.close()
+    # Obtain band structure
+    nao = Al_G.shape[1]
+    nk  = Al_G.shape[2]
+    DOS_G_dlr = np.zeros((freqs.size, nk, nao), dtype=np.float_)
+    for k in range(nk):
+        DOS_G_dlr[:, k, :] = -1.0 / np.pi * cal_G_vector(freqs + 1j * eta, Al_G[:np_G[k], :, k], xl_G[:np_G[k], k]).imag
+    band = DOS_G_dlr.sum(axis=2)
+else:
+    # Read complex pole information
+    data = h5py.File("pole_S.h5", "r")
+    Al_S = data["Al"][:]
+    xl_S = data["xl"][:]
+    np_S = data["np"][:]
+    data.close()
+    # Obtain retarded self-energy
+    nao = Al_S.shape[1]
+    nk  = Al_S.shape[3]
+    S_ret_dlr = np.zeros((freqs.size, nk, nao, nao), dtype=np.complex_)
+    for k in range(nk):
+        S_ret_dlr[:, k, :, :] = cal_G_vector(freqs + 1j * eta,  Al_S[:np_S[k], :, :, k].reshape(-1, nao**2), xl_S[:np_S[k], k]).reshape(-1, nao, nao)
+    # Perform Dyson equation above the real axis
+    G0_ret_inv = ((freqs + 1j * eta).reshape(-1, 1, 1, 1) * np.eye(nao).reshape(1, 1, nao, nao) + mu * np.eye(nao).reshape(1, 1, nao, nao) - Fk.reshape(1, nk, nao, nao))
+    G_ret = np.zeros_like(S_ret_dlr)
+    for i in range(G_ret.shape[0]):
+        for j in range(G_ret.shape[1]):
+            G_ret[i, j] = np.linalg.pinv(G0_ret_inv[i, j] - S_ret_dlr[i, j])
+    # Obtain band structure
+    band = (-1.0 / np.pi * np.einsum("ijkk->ijk", G_ret[:, :, :, :]).imag).sum(axis=2)
+
+# Plot band structure
+plt.figure(figsize=(10, 6))
+plt.imshow(band, aspect='auto', origin='lower', extent=[path[0], path[-1], y[0], y[-1]], cmap="RdBu", vmin=0, vmax=1 / eta)
+plt.xticks(sp_points, sp_labels)
+plt.tick_params(labelsize=20)
+plt.ylabel(r'$\omega$(eV)', fontsize=20)
+title = "G_{ii}" if obs == "Gii" else "G" if obs == "G" else "S"
+plt.title(r"from $" + title + "$", fontsize=20)
+cbar = plt.colorbar()
+cbar.ax.tick_params(labelsize=20)
+plt.savefig("Si_dlr_" + obs + ".pdf")
+plt.show()
